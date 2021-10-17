0f42029ba366d02d9f913adce7b12aa33d7a9997
Shapon Sheikh <aptshapon@hotmail.com>
2021-10-17 10:18:09 +0600

    final version ffmpeg multithreadings

--- ffmpeg_multithread.py
+++ ffmpeg_multithread.py
@@ -1,44 +1,49 @@
 from ffmpeg_streaming import Formats
-import ffmpeg_streaming, logging, time
-from threading import *
-from concurrent.futures import ThreadPoolExecutor
+import ffmpeg_streaming
+import time
+from threading import current_thread
+from concurrent.futures import ThreadPoolExecutor, as_completed
+from pathlib import Path
 
 
 class Transcode:
 
-    def hls(self, video, thread):
+    def hls(self, key, value):
         start_time = time.perf_counter()
-        logging.basicConfig(level=logging.DEBUG)
-        hls_video = ffmpeg_streaming.input(video)
+        hls_video = ffmpeg_streaming.input(value)
         hls = hls_video.hls(Formats.h264(), hls_time=5.5, start_number=0)
         hls.auto_generate_representations([1080, 720, 480, 360, 240, 144])
-        hls.output(f"/Users/ss/Desktop/djax/output/hls/{thread}/hls.m3u8")
+        hls.output(f"{Path.cwd()}/output/hls/{key}/hls.m3u8")
         print("Executing thread name :", current_thread().getName())
         print(f"--- HLS finished in: {round(time.perf_counter() - start_time, 2)} seconds ---")
 
-    def dash(self, video, thread):
+    def dash(self, key, value):
         start_time = time.perf_counter()
-        dash_video = ffmpeg_streaming.input(video)
+        dash_video = ffmpeg_streaming.input(value)
         dash = dash_video.dash(Formats.h264())
         dash.auto_generate_representations([1080, 720, 480, 360, 240, 144])
-        dash.output(f"/Users/ss/Desktop/djax/output/dash/{thread}/dash.mpd")
+        dash.output(f"{Path.cwd()}/output/dash/{key}/dash.mpd")
         print("Executing thread name:", current_thread().getName())
-        print(f"--- DASH finished in: {round(time.perf_counter() - start_time, 2)} seconds ---")
+        print(
+            f"--- DASH finished in: {round(time.perf_counter() - start_time, 2)} seconds ---")
 
 
-def main():
+def main(videos):
     transcode = Transcode()
-    video_list = ['30_sec_1080p_wc.mp4', '2nd_video.mp4']
-    with ThreadPoolExecutor(max_workers=4) as executor:
-        for list in video_list:
-            executor.submit(transcode.hls, list, video_list.index)
-        # executor.submit(transcode.hls, playlist[1], 'hls_2')
-        # executor.submit(transcode.hls, '30_sec_1080p_wc.mp4', 'hls_2')
-        # executor.submit(transcode.dash, '30_sec_1080p_wc.mp4', 'dash_1')
-        # executor.submit(transcode.dash, '30_sec_1080p_wc.mp4', 'dash_2')
+    with ThreadPoolExecutor(max_workers=len(videos)) as executor:
+        hls_transcode = {executor.submit(transcode.hls, k, v): (k, v) for k, v in videos.items()}
+        for hls in as_completed(hls_transcode):
+            k, v = hls_transcode[hls]
+            print('k: ', k, 'v: ', v)
 
+        dash_transcode = {executor.submit(transcode.dash, k, v): (k, v) for k, v in videos.items()}
+        for dash in as_completed(dash_transcode):
+            k, v = dash_transcode[dash]
+            print('k: ', k, 'v: ', v)
 
 if __name__ == "__main__":
-    main()
-
-print("Master Thread: ", current_thread().getName())
+    video_lists = {
+        0 : '30_sec_1080p_wc.mp4',
+        1 : '30_sec_1080p_wc.mp4',
+    }
+    main(video_lists)
