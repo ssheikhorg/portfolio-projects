8f01dd1f413c0c24fa1afbe44ffe5f88bb9755e9
Shapon Sheikh <aptshapon@hotmail.com>
2021-10-18 17:46:57 +0600

    Separated in functions from a class

--- ffmpeg_multithread.py
+++ ffmpeg_multithread.py
@@ -6,39 +6,34 @@ from concurrent.futures import ThreadPoolExecutor, as_completed
 from pathlib import Path
 
 
-class Transcode:
+def hls_func(key, value):
+    start_time = time.perf_counter()
+    hls_video = ffmpeg_streaming.input(value)
+    hls = hls_video.hls(Formats.h264(), hls_time=5.5, start_number=0)
+    hls.auto_generate_representations([1080, 720, 480, 360, 240, 144])
+    hls.output(f"{Path.cwd()}/output/hls/{key}/hls.m3u8")
+    print("Executing thread name :", current_thread().getName())
+    print(f"--- HLS finished in: {round(time.perf_counter() - start_time, 2)} seconds ---")
 
-    @staticmethod
-    def hls(key, value):
-        start_time = time.perf_counter()
-        hls_video = ffmpeg_streaming.input(value)
-        hls = hls_video.hls(Formats.h264(), hls_time=5.5, start_number=0)
-        hls.auto_generate_representations([1080, 720, 480, 360, 240, 144])
-        hls.output(f"{Path.cwd()}/output/hls/{key}/hls.m3u8")
-        print("Executing thread name :", current_thread().getName())
-        print(f"--- HLS finished in: {round(time.perf_counter() - start_time, 2)} seconds ---")
 
-    @staticmethod
-    def dash(key, value):
-        start_time = time.perf_counter()
-        dash_video = ffmpeg_streaming.input(value)
-        dash = dash_video.dash(Formats.h264())
-        dash.auto_generate_representations([1080, 720, 480, 360, 240, 144])
-        dash.output(f"{Path.cwd()}/output/dash/{key}/dash.mpd")
-        print("Executing thread name:", current_thread().getName())
-        print(
-            f"--- DASH finished in: {round(time.perf_counter() - start_time, 2)} seconds ---")
+def dash_func(key, value):
+    start_time = time.perf_counter()
+    dash_video = ffmpeg_streaming.input(value)
+    dash = dash_video.dash(Formats.h264())
+    dash.auto_generate_representations([1080, 720, 480, 360, 240, 144])
+    dash.output(f"{Path.cwd()}/output/dash/{key}/dash.mpd")
+    print("Executing thread name:", current_thread().getName())
+    print(
+        f"--- DASH finished in: {round(time.perf_counter() - start_time, 2)} seconds ---")
 
 
 def main(videos):
-    transcode = Transcode()
     with ThreadPoolExecutor(max_workers=len(videos)) as executor:
-        hls_transcode = {executor.submit(transcode.hls, k, v): (k, v) for k, v in videos.items()}
+        hls_transcode = {executor.submit(hls_func, k, v): (k, v) for k, v in videos.items()}
         for hls in as_completed(hls_transcode):
             k, v = hls_transcode[hls]
-            print('k: ', k, 'v: ', v)
 
-        dash_transcode = {executor.submit(transcode.dash, k, v): (k, v) for k, v in videos.items()}
+        dash_transcode = {executor.submit(dash_func, k, v): (k, v) for k, v in videos.items()}
         for dash in as_completed(dash_transcode):
             k, v = dash_transcode[dash]
             print('k: ', k, 'v: ', v)
