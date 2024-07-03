e45955b92f6c6e03e2a8dde1005f73dbdaeb3e9b
kalebda <kalebu246@gmail.com>
2024-07-03 23:04:12 +0300

    issue token route

---
+++ backend/api/issue_token_routes.py
@@ -0,0 +1,19 @@
+from fastapi import APIRouter, HTTPException, Query
+from utils.issue_token import create_jwt_token
+
+app = APIRouter()
+
+
+@app.post("/issueToken")
+async def issue_token(api_key: str = Query(...)):
+    """
+    Issues a JWT token from the given api key
+
+    """
+    try:
+        jwt_token = create_jwt_token(api_key)
+        return {"status": "success", "token": jwt_token}
+    except HTTPException as http_exception:
+        raise http_exception
+    except Exception as e:
+        raise HTTPException(status_code=500, detail="Internal server error")
