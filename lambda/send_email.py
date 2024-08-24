b0bb81ca8ab1e66f756679554611aca20913deb1
Shapon Sheikh <shsheikhbd@gmail.com>
2024-08-24 14:00:42 +0600

    added send email lambda

---
+++ lambda/send_email.py
@@ -0,0 +1,36 @@
+import json
+import boto3
+
+ses_client = boto3.client('ses')
+
+def handler(event, context):
+    # Extract information from the event
+    processing_result = event['processing_successful']
+    message = event['message']
+    email_subject = "File Processing Status"
+    email_body = f"The file was processed with the following result: {message}"
+
+    # Send email
+    response = ses_client.send_email(
+        Source="your-email@example.com",
+        Destination={
+            "ToAddresses": ["user@example.com"]
+        },
+        Message={
+            "Subject": {
+                "Data": email_subject
+            },
+            "Body": {
+                "Text": {
+                    "Data": email_body
+                }
+            }
+        }
+    )
+
+    return {
+        "statusCode": 200,
+        "body": json.dumps({
+            "email_status": "Sent"
+        })
+    }
