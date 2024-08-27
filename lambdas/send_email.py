a1efba9f23dc10eab55316c7da0c835f19a0cf61
Shapon Sheikh <shsheikhbd@gmail.com>
2024-08-27 11:40:26 +0600

    fixed email issue

--- lambdas/send_email.py
+++ lambdas/send_email.py
@@ -1,38 +1,43 @@
-import json
 import boto3
 
 ses_client = boto3.client('ses')
 
 
-def handler(event, context):
-    print("Send Email Event: ", event)
-    processing_successful = event["body"]["valid"]
-    message = event["body"]["message"]
-    email_subject = "File Processing Status"
-    email_body = f"The file was processed with the following result: {message}"
+def send_email(subject: str, body: str) -> dict:
+    source_email = "ssheikhorg@hotmail.com"
+    destination_email = "shsheikhbd@gmail.com"
+    response = ses_client.send_email(
+        Source=source_email,
+        Destination={
+            "ToAddresses": [destination_email]
+        },
+        Message={
+            "Subject": {
+                "Data": subject
+            },
+            "Body": {
+                "Text": {
+                    "Data": body
+                }
+            }
+        }
+    )
+    return response
 
-    # # Send email
-    # response = ses_client.send_email(
-    #     Source="shsheikhbd@gmail.com",
-    #     Destination={
-    #         "ToAddresses": ["shsheikhbd@gmail.com"]
-    #     },
-    #     Message={
-    #         "Subject": {
-    #             "Data": email_subject
-    #         },
-    #         "Body": {
-    #             "Text": {
-    #                 "Data": email_body
-    #             }
-    #         }
-    #     }
-    # )
+
+def handler(event, _) -> dict:
+    print("Send Email Event: ", event)
+    email_subject = "File Processing Status"
+
+    if "Payload" not in event:
+        send_email(email_subject, "The file processing failed.")
+        return {
+            "statusCode": 400,
+            "valid": False
+        }
+    send_email(email_subject, "The file processing was successful.")
 
     return {
         "statusCode": 200,
-        "body": json.dumps({
-            "valid": processing_successful,
-            "message": "Email sent successfully"
-        })
+        "valid": True
     }
