f4545af928abd8d369d89f63940b3c9b35b3b887
Shapon Sheikh <shsheikhbd@gmail.com>
2024-09-23 00:41:44 +0600

    added global_exception.py

---
+++ backend/utils/global_exception.py
@@ -0,0 +1,29 @@
+from fastapi import HTTPException
+import traceback
+
+from utils.log_function import get_logger
+
+logger, memory_handler = get_logger("File Processing")
+
+
+def handle_global_exception(e: Exception) -> None:
+    exception_map = {
+        HTTPException: (e.status_code, e.detail),
+        ValueError: (422, str(e)),
+        IOError: (400, str(e)),
+    }
+
+    # Get the status code and detail for the exception type, or use a default for unknown exceptions
+    status_code, detail = exception_map.get(
+        type(e), (500, f"An unexpected error occurred: {str(e)}")
+    )
+    if isinstance(e, HTTPException):
+        logger.error(f"HTTP exception occurred: {detail}")
+        logger.debug(f"HTTP exception status code: {status_code}")
+    else:
+        severity_level = "error" if status_code < 500 else "critical"
+        log_function = getattr(logger, severity_level)
+        log_function(detail)
+        logger.debug(f"Exception type: {type(e).__name__}, Traceback: {traceback.format_exc()}")
+
+    raise HTTPException(status_code=status_code, detail=detail)
